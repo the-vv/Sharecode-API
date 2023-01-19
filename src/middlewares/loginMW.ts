12a2874b13a735e411f9ba5af385b5cd1f79940b
Vishnu Vinod <vishnuvinod2772001@gmail.com>
2023-01-19 23:53:50 +0530

    added a middleware

--- src/middlewares/loginMW.ts
+++ src/middlewares/loginMW.ts
@@ -1,5 +1,22 @@
+import { verifyJwt } from "@/services/jwt-service";
+import { appErrorJson } from "@/utils/helper-functions";
 import { NextFunction, Request, Response } from "express";
+import { ReasonPhrases } from "http-status-codes";
+import { StatusCodes } from "http-status-codes/build/cjs/status-codes";
 
-export const loginMW = (req: Request, res: Response, next: NextFunction) => {
-
+export const loginMW = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        // Get auth token from header bearer token
+        const token = req.headers.authorization;
+        // Get json-web-token
+        const jwt = token?.split(' ')?.[1];
+        if (!jwt) {
+            return res.status(StatusCodes.UNAUTHORIZED).json(appErrorJson(ReasonPhrases.UNAUTHORIZED));
+        }
+        const clientData = await verifyJwt(jwt);
+        res.locals.currentUser = clientData;
+        next();
+    } catch (err: any) {
+        return res.status(StatusCodes.UNAUTHORIZED).json(appErrorJson(String(err?.message || err)));
+    }
 }
