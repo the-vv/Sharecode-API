20f52de9835da89b87171d46533adf8308c88914
Vishnu Vinod <vishnuvinod2772001@gmail.com>
2023-07-12 21:02:59 +0530

    some rearrangements

--- src/routes/snippet/router.ts
+++ src/routes/snippet/router.ts
@@ -8,66 +8,12 @@ import { appErrorJson } from "@/utils/helper-functions";
 
 const router = express.Router();
 
-router.post('/', async (req, res) => {
-    const body = snippetSchema.parse(req.body);
-    const snippet = await SnippetController.createOne(body);
-    res.json(snippet);
-})
-
 router.get('/my-snippets', async (req, res) => {
     const body = ListSchema.extend({
         userId: z.string()
     }).parse(req.body);
     const userSnippets = await SnippetController.getByUserId(body.userId, body);
     res.json(userSnippets);
-})
-
-router.post('/trending', async (req, res) => {
-    const body = ListSchema.extend({
-        userId: z.string()
-    }).parse(req.body);
-    const trendingSnippets = await SnippetController.getTrending(body);
-    res.json(trendingSnippets);
-})
-
-router.put('/:id/comment', async (req, res) => {
-    const body = CommentsSchema.parse(req.body);
-    const snippet = await SnippetController.addComment(req.params.id, body);
-    res.json(snippet);
-})
-
-router.delete('/:id/comment/:commentId', async (req, res) => {
-    if (!req.params.commentId) {
-        return res.status(400).json(appErrorJson('commentId is required'));
-    }
-    if (!appConfigs.mongoDBIdRegexp.test(req.params.commentId)) {
-        return res.status(400).json(appErrorJson('commentId is invalid'));
-    }
-    const snippet = await SnippetController.removeComment(req.params.id, req.params.commentId);
-    res.json(snippet);
-})
-
-router.delete('/:id/like/:userId', async (req, res) => {
-    if (!req.params.userId) {
-        return res.status(400).json(appErrorJson('userId is required'));
-    }
-    if (!appConfigs.mongoDBIdRegexp.test(req.params.userId)) {
-        return res.status(400).json(appErrorJson('userId is invalid'));
-    }
-    const snippet = await SnippetController.removeLike(req.params.id, req.params.userId);
-    res.json(snippet);
-})
-
-
-router.put('/:id/like', async (req, res) => {
-    if (!req.body.userId) {
-        return res.status(400).json(appErrorJson('userId is required'));
-    }
-    if (!appConfigs.mongoDBIdRegexp.test(req.body.userId)) {
-        return res.status(400).json(appErrorJson('userId is invalid'));
-    }
-    const snippet = await SnippetController.addLike(req.params.id, req.body.userId);
-    res.json(snippet);
 })
 
 router.get('/:id', async (req, res) => {
@@ -82,6 +28,37 @@ router.get('/:id/likes', async (req, res) => {
 
 router.get('/:id/comments', async (req, res) => {
     const snippet = await SnippetController.getComments(req.params.id);
+    res.json(snippet);
+})
+
+router.post('/trending', async (req, res) => {
+    const body = ListSchema.extend({
+        userId: z.string()
+    }).parse(req.body);
+    const trendingSnippets = await SnippetController.getTrending(body);
+    res.json(trendingSnippets);
+})
+
+router.post('/', async (req, res) => {
+    const body = snippetSchema.parse(req.body);
+    const snippet = await SnippetController.createOne(body);
+    res.json(snippet);
+})
+
+router.put('/:id/comment', async (req, res) => {
+    const body = CommentsSchema.parse(req.body);
+    const snippet = await SnippetController.addComment(req.params.id, body);
+    res.json(snippet);
+})
+
+router.put('/:id/like', async (req, res) => {
+    if (!req.body.userId) {
+        return res.status(400).json(appErrorJson('userId is required'));
+    }
+    if (!appConfigs.mongoDBIdRegexp.test(req.body.userId)) {
+        return res.status(400).json(appErrorJson('userId is invalid'));
+    }
+    const snippet = await SnippetController.addLike(req.params.id, req.body.userId);
     res.json(snippet);
 })
 
@@ -110,5 +87,28 @@ router.delete('/:id/force', async (req, res) => {
     const snippet = await SnippetController.hardDeleteById(req.params.id);
     res.json(snippet);
 })
+
+router.delete('/:id/like/:userId', async (req, res) => {
+    if (!req.params.userId) {
+        return res.status(400).json(appErrorJson('userId is required'));
+    }
+    if (!appConfigs.mongoDBIdRegexp.test(req.params.userId)) {
+        return res.status(400).json(appErrorJson('userId is invalid'));
+    }
+    const snippet = await SnippetController.removeLike(req.params.id, req.params.userId);
+    res.json(snippet);
+})
+
+router.delete('/:id/comment/:commentId', async (req, res) => {
+    if (!req.params.commentId) {
+        return res.status(400).json(appErrorJson('commentId is required'));
+    }
+    if (!appConfigs.mongoDBIdRegexp.test(req.params.commentId)) {
+        return res.status(400).json(appErrorJson('commentId is invalid'));
+    }
+    const snippet = await SnippetController.removeComment(req.params.id, req.params.commentId);
+    res.json(snippet);
+})
+
 
 export const snippetRouter = router;
